fb6fc65ae647fe3a72fc57695897332bf7da83ed
Karen Knup <106675809+KarenKnup@users.noreply.github.com>
2023-08-09 19:30:37 -0300

    Update main(3).py

--- main(3).py
+++ main(3).py
@@ -64,3 +64,13 @@ Make the following operation result in True under these conditions
 
 presence and (temp > 30)
 
+Exemplo:
+# Take steps and minutes as inputs
+steps = int(input())
+active_minutes = int(input())
+
+# Store the result of the operations in the variable
+goal_achieved = (steps > 10000) or (active_minutes > 30)
+
+# Display the result on the screen
+print(goal_achieved)
